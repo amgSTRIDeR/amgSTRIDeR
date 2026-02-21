fb2e99869b9725d86a6c7282ae8438f6fba1c57a
amgstrider <amgstrider@gmail.com>
2026-02-21 18:12:32 +0400

    refactor: svg wrapper base64

--- .github/scripts/generate-svg-wrappers.js
+++ .github/scripts/generate-svg-wrappers.js
@@ -21,9 +21,14 @@ async function generateSvgWrappers() {
     const height = IMAGE_HEIGHT + BORDER_SIZE * 2
 
     for (const file of webpFiles) {
+      const filePath = path.join(thumbnailsDir, file)
+      const imageBuffer = await fs.readFile(filePath)
+      const base64 = imageBuffer.toString('base64')
+      const dataUri = `data:image/webp;base64,${base64}`
+
       const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
   <rect width="100%" height="100%" fill="${BORDER_COLOR}"/>
-  <image xlink:href="${file}" x="${BORDER_SIZE}" y="${BORDER_SIZE}" width="${IMAGE_WIDTH}" height="${IMAGE_HEIGHT}"/>
+  <image href="${dataUri}" x="${BORDER_SIZE}" y="${BORDER_SIZE}" width="${IMAGE_WIDTH}" height="${IMAGE_HEIGHT}"/>
 </svg>`
 
       const svgFilename = file.replace('.webp', '.svg')
